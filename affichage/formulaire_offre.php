ffb018ebfe62d09b72c1050538b5893f24021840
LionelKb <lionel.kubwimana@gmail.com>
2015-12-17 12:17:12 +0100

    Indentation du code pour la lisibilité

--- affichage/formulaire_offre.php
+++ affichage/formulaire_offre.php
@@ -26,43 +26,48 @@ if(isset($_POST['btn-save']))
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>CALL FOR LIQUIDITY</title>
-<link rel="stylesheet" href="style3.css" type="text/css" />
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<title>CALL FOR LIQUIDITY</title>
+	<link rel="stylesheet" href="style3.css" type="text/css" />
 </head>
 <body>
-<center>
-<div id="login-form">
-<form method="post">
-<table align="center" width="30%" border="0">
-<tr>
-<td><label><select name="bank"><option value="1">Banque de Crédit de Bujumbura</option>
-		                        <option value="2">Banque Commerciale du Burundi</option>
-							    <option value="3">Banque Burundaise pour le Commerce et l'Investissement</option>
-								<option value="4">Interbank Burundi</option>
-								<option value="5">Banque de Gestion et de financement</option>
-								<option value="6">Ecobank</option>
-								<option value="7">Finbank</option>
-								<option value="8">DTB Burundi</option>
-								<option value="9">KCB Burundi</option>
-								<option value="10">CRDB Burundi</option>
-	                            </select></label></td>
-</tr>
-<tr>
-<td><input type="float" name="rate" placeholder="Taux" required min="0"/></td>
-</tr>
-<tr>
-<td><input type="number" name="amount" placeholder="Montant de l'offre" required min="0"/></td>
-</tr>
-<tr>
-<td><button type="submit" name="btn-save">ENREGISTRER</button></td>
-</tr>
-<tr>
-<td><button id="work"><a href="list2.php">Terminer</a></button></td>
-</tr>
-</table>
-</form>
-</div>
-</center>
+	<center>
+		<div id="login-form">
+			<form method="post">
+				<table align="center" width="30%" border="0">
+					<tr>
+						<td>
+							<label>
+								<select name="bank">
+									<option value="1">Banque de Crédit de Bujumbura</option>
+									<option value="2">Banque Commerciale du Burundi</option>
+									<option value="3">Banque Burundaise pour le Commerce et l'Investissement</option>
+									<option value="4">Interbank Burundi</option>
+									<option value="5">Banque de Gestion et de financement</option>
+									<option value="6">Ecobank</option>
+									<option value="7">Finbank</option>
+									<option value="8">DTB Burundi</option>
+									<option value="9">KCB Burundi</option>
+									<option value="10">CRDB Burundi</option>
+								</select>
+							</label>
+						</td>
+					</tr>
+					<tr>
+						<td><input type="float" name="rate" placeholder="Taux" required min="0"/></td>
+					</tr>
+					<tr>
+						<td><input type="number" name="amount" placeholder="Montant de l'offre" required min="0"/></td>
+					</tr>
+					<tr>
+						<td><button type="submit" name="btn-save">ENREGISTRER</button></td>
+					</tr>
+					<tr>
+						<td><button id="work"><a href="list2.php">Terminer</a></button></td>
+					</tr>
+				</table>
+			</form>
+		</div>
+	</center>
 </body>
 </html>
